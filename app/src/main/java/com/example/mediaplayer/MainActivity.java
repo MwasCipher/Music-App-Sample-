5f0f14119ac95b144e6433797ab953ca57dc3d1e
MwasCipher <mwangicollinswaweru@gmail.com>
2020-01-02 19:03:14 +0300

    Set Up Media Play Buttons

--- app/src/main/java/com/example/mediaplayer/MainActivity.java
+++ app/src/main/java/com/example/mediaplayer/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.mediaplayer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //initialize media buttons
+
+        Button playButton = findViewById(R.id.play_button);
+        Button pauseButton = findViewById(R.id.pause_button);
+
+        //when play button is clicked it initiates media player
+
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        //pauses media play
+        pauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
