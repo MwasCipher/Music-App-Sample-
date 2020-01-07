48545ebf3426d48d61c99da5d6cf263fb86aa81c
MwasCipher <mwangicollinswaweru@gmail.com>
2020-01-07 20:49:05 +0300

    Async Tasks

--- app/src/main/java/com/example/mediaplayer/MainActivity.java
+++ app/src/main/java/com/example/mediaplayer/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.mediaplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,18 +20,30 @@ public class MainActivity extends AppCompatActivity {
         Button playButton = findViewById(R.id.play_button);
         Button pauseButton = findViewById(R.id.pause_button);
 
+        //Instantiate Media Player Class
+
+        final MediaPlayer mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.hate_me);
+
         //when play button is clicked it initiates media player
 
         playButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
+                mediaPlayer.start();
+                mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    @Override
+                    public void onCompletion(MediaPlayer mp) {
+                        Toast.makeText(MainActivity.this, "Done Playing", Toast.LENGTH_LONG).show();
+                    }
+                });
             }
         });
         //pauses media play
         pauseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                mediaPlayer.pause();
 
             }
         });
